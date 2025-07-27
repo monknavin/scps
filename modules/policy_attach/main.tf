66013ddde193109bd400afb67df7901e82475a81
monknavin <navinwork21@gmail.com>
2025-07-27 17:28:14 +0530

    Delete modules/policy_attach directory

--- modules/policy_attach/main.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_organizations_policy_attachment" "this" {
-  for_each  = toset(var.policies)
-  policy_id = var.policy_id["${var.policies_directory_name}/${each.value}.json"]
-  target_id = var.ou
-}
